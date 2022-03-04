8e249ae875147bd298f96d35871bffab43091b8e
uzzalcontact <uzzalcontact@gmail.com>
2022-03-04 11:36:11 +0600

    ui-splash

---
+++ app/src/main/java/com/blood/blooddonorapp/splash/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.blood.blooddonorapp.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.blood.blooddonorapp.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private val time: Long = 2000;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        showSplash()
+    }
+
+    private fun showSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            goHome()
+        },time)
+    }
+
+    private fun goHome() {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+}
