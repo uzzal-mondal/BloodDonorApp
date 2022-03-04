da9174ccd38a70e50ff34ca59bbbf78462a53829
uzzalcontact <uzzalcontact@gmail.com>
2022-03-04 16:43:09 +0600

    done ui bottom nav

--- app/src/main/java/com/blood/blooddonorapp/MainActivity.kt
+++ app/src/main/java/com/blood/blooddonorapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.blood.blooddonorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.blood.blooddonorapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showBottomNav()
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavView.background = null
+        //bottomNav.menu.getItem(3).isEnabled = false
+        navController = findNavController(R.id.fragment_container_view)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home_fragment, R.id.profile_fragment,
+                R.id.bookmark_fragment, R.id.notification_fragment
+            )
+        )
+        binding.bottomNavView.setupWithNavController(navController)
+
     }
 }
