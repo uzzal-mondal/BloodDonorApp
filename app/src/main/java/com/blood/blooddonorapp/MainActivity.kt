045a3658dd34b5eba76276e619b4833ac5b8f6bd
Uzzal <uzzalcontact@gmail.com>
2022-03-07 16:29:06 +0600

    done date and time

--- app/src/main/java/com/blood/blooddonorapp/MainActivity.kt
+++ app/src/main/java/com/blood/blooddonorapp/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.blood.blooddonorapp
 
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.ArrayAdapter
 import android.widget.Toast
-
-import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -19,14 +19,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.blood.blooddonorapp.databinding.ActivityMainBinding
-
-
+import com.blood.blooddonorapp.databinding.DialogUserDetailsBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     // private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var dUserBinding: DialogUserDetailsBinding
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
@@ -80,23 +80,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showUserDetailsDialog() {
+
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dialog_user_details)
-        val close = dialog.findViewById<ImageView>(R.id.btn_close)
-        val submitBtn = dialog.findViewById<Button>(R.id.btn_submit)
-
-        val etPatient = dialog.findViewById<TextInputEditText>(R.id.et_patient)
-        val bloodGroup = dialog.findViewById<AutoCompleteTextView>(R.id.auto_text_blood_group)
-        val etBloodBag = dialog.findViewById<TextInputEditText>(R.id.et_blood_bag)
-        val etHospital = dialog.findViewById<TextInputEditText>(R.id.et_hospital)
+        dUserBinding = DialogUserDetailsBinding.inflate(layoutInflater)
+        dialog.setContentView(dUserBinding.root)
 
 
-
-
-        close?.setOnClickListener {
+        dUserBinding.btnClose.setOnClickListener {
             if (dialog.isShowing) {
                 dialog.dismiss()
             }
@@ -108,19 +101,106 @@ class MainActivity : AppCompatActivity() {
         )
         //
         if (!dialog.isShowing) {
-            submitBtn?.setOnClickListener {
+            showBloodGp()
+
+            dUserBinding.textDate.setOnClickListener {
+                showDateDialog()
+            }
+
+            dUserBinding.textTime.setOnClickListener {
+                showTimeDialog()
+            }
+
+            dUserBinding.btnSubmit.setOnClickListener {
+                val patient = dUserBinding.etPatient.text.toString()
+                val bloodGp = dUserBinding.autoTextBloodGroup.text.toList()
+                val bloodBag = dUserBinding.etBloodBag.text.toString()
+                val date = dUserBinding.textDate.text.toString()
+                val time = dUserBinding.textTime.text.toString()
+                val hospital = dUserBinding.etHospital.text.toString()
+                val district = dUserBinding.etDistrict.text.toString()
+                val contact = dUserBinding.etContact.text.toString()
 
 
+                when {
+                    patient.isEmpty() -> {
+                        dUserBinding.etPatient.error = "patient name required"
+                    }
+                    bloodGp.isEmpty() -> {
+                        dUserBinding.autoTextBloodGroup.error = "patient name required"
+                    }
+                    bloodBag.isEmpty() -> {
+                        dUserBinding.etBloodBag.error = "patient name required"
+                    }
+                    date.isEmpty() -> {
+                        dUserBinding.textDate.error = "patient name required"
+                    }
+                    time.isEmpty() -> {
+                        dUserBinding.textTime.error = "patient name required"
+                    }
+                    hospital.isEmpty() -> {
+                        dUserBinding.etHospital.error = "patient name required"
+                    }
+                    district.isEmpty() -> {
+                        dUserBinding.etDistrict.error = "patient name required"
+                    }
+                    contact.isEmpty() -> {
+                        dUserBinding.etContact.error = "patient name required"
+                    }
+                    else -> {
+                        Toast.makeText(this, "success.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
             dialog.show()
         }
-
     }
 
-    private fun validateSignIn() {
+    private fun showBloodGp() {
+        var blGp = arrayOf("A+", "B+", "O+", "AB+", "A-", "B-", "O-", "AB-")
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, blGp)
+        dUserBinding.autoTextBloodGroup.threshold = 0
+        dUserBinding.autoTextBloodGroup.setAdapter(adapter)
+        dUserBinding.autoTextBloodGroup.setOnFocusChangeListener { view, b ->
+            if (b) dUserBinding.autoTextBloodGroup.showDropDown()
+        }
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun showDateDialog() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in textbox
+                dUserBinding.textDate.setText("" + dayOfMonth + " " + monthOfYear + ", " + year)
+            },
+            year,
+            month,
+            day
+        )
+
+        dpd.show()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showTimeDialog() {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR)
+        val minute = c.get(Calendar.MINUTE)
+        val tpd =
+            TimePickerDialog(this, TimePickerDialog.OnTimeSetListener(function = { view, h, m ->
+                dUserBinding.textTime.text = "$h : $m : "
+            }), hour, minute, false)
+        tpd.show()
     }
 }
+
+
 
 
 
